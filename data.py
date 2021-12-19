0399d94d8af9ed8f70c09cf9bea109b01c2b4d6f
Rob Howard <rob@willo.org>
2021-12-18 17:37:54 -0700

    Fluid objects

---
+++ data.py
@@ -0,0 +1,68 @@
+# http://pythex.org/
+import unittest
+import math
+import re
+import random
+from tools import *
+
+class Data (Fluid):
+  """
+  This class wraps the entire data.yaml file. You can substitute your own if needed.
+  But this class ensures that the yaml conforms to expectations.
+  """
+
+  def __init__(self, data = None):
+    """
+    @data is the object returned from calling:
+      data = ReadYaml("data.yaml")
+    """
+    if not data:
+      data = ReadYaml("data.yaml")
+    self.data = data
+    
+  def GetAbility(self, key):
+    """
+    @key can be any string that matches an ability's abbr, symbol, or name value.
+    EXAMPLE:
+    ability = data.GetAbility("str")
+    ASSERT: ability == 
+    """
+    key = key.lower();
+    for ability in self.data['abilities']:
+      if ability['abbr'].lower() == key: return ability
+      if ability['symbol'].lower() == key: return ability
+      if ability['name'].lower() == key: return ability
+    raise Exception(f"Ability '{key}' not found.")
+
+
+class DataTests(unittest.TestCase):
+  def test_abilities(self):
+    data = Data()
+    ability = data.GetAbility("s")
+    self.assertEqual("Strength", ability['name'])
+    ability = data.GetAbility("S")
+    self.assertEqual("Strength", ability['name'])
+    ability = data.GetAbility("STRENGTH")
+    self.assertEqual("Strength", ability['name'])
+    ability = data.GetAbility("Str")
+    self.assertEqual("Strength", ability['name'])
+
+    ability = data.GetAbility("i")
+    self.assertEqual("Intelligence", ability['name'])
+
+    ability = data.GetAbility("w")
+    self.assertEqual("Wisdom", ability['name'])
+
+    ability = data.GetAbility("d")
+    self.assertEqual("Dexterity", ability['name'])
+
+    ability = data.GetAbility("c")
+    self.assertEqual("Constitution", ability['name'])
+
+    ability = data.GetAbility("h")
+    self.assertEqual("Charisma", ability['name'])
+
+    self.assertRaises(Exception, data.GetAbility, "xyzzy")
+
+if __name__ == '__main__':
+    unittest.main()
--- weapon.py
+++ weapon.py
@@ -1,31 +1,41 @@
 # http://pythex.org/
 import unittest
+from tools import *
+from money import Money
 
 
 class Weapon:
+
   def __init__(self, obj):
     """
     @obj is the yaml object that represents this class.
     """
     self.obj = obj
     self.name = obj['name']
-    self.cost = Cost()
+    self.properties = obj['properties']
+    self.cost = Money(obj["cost"])
+    self.damage = Damage(obj["damage"])
+    self.weight = Damage(obj["weight"])
 
-  def Role(self):
-    n = 0
-    for i in range(self.N):
-      n += random.randint(1, self.D)
-    if self.SIGN == "+":
-        n += self.A
-    else:
-        n -= self.A
-    return n
+    # Validation
+
 
 
 
 class WeaponTests(unittest.TestCase):
-  def test_1(self):
-    pass
+  def test_load(self):
+    yaml = '''
+club:
+  name: Club
+  cost: 1sp
+  damage: 1d4 bludgeoning
+  weight: 2 lbs
+  properties: light
+    '''
+    y = ParseYaml(yaml)
+    weapon = Weapon(y['club'])
+    self.assertEqual(10, weapon.cost.Dollars())
+    self.assertEqual(10, weapon.damage)
 
 if __name__ == '__main__':
     unittest.main()
